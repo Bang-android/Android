8df8ca692b7c93f6b88c3e7c053c305fe70a4bfe
Bang-android <78091751+Bang-android@users.noreply.github.com>
2021-02-01 18:17:35 +0900

    Update selectEquipment.php

--- Android_Project/PHP/selectEquipment.php
+++ Android_Project/PHP/selectEquipment.php
@@ -2,6 +2,7 @@
     if ($_SERVER['REQUEST_METHOD'] == 'GET') {
       include('dbcon.php');
 
+        // 결과를 받았다고 가정해서 작성한 것!
       $stmt = $con -> prepare("SELECT * FROM equipment WHERE name = 'arm curl machine' OR name = 'bench press machine' OR name = 'cable crossover machine'");
 
       $result = array();
